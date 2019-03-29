983d79b695d60be0fd010abb0cbb47e7164ca923
Ronnie Gross <rnngrss1495@gmail.co>
2019-03-29 09:17:28 -0400

    fixed mobile nav bs

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -21,7 +21,7 @@ class NavBar extends Component {
                 <Wrapper>
                     <nav>
                         <div className="nav-wrapper">
-                            <ul id="nav-mobile" className="left hide-on-med-and-down">
+                            <ul className="left hide-on-med-and-down">
                                 <li><Link to={`/${this.props.userId}/cheeses`}> cheese list </Link></li>
                                 <li><Link to={`/${this.props.userId}`}> account info </Link></li>
                             </ul>
